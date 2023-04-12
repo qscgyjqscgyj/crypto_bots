76d183340f08dd9714b41dd7927e9e5e8e8b8fb8
@MikhailNikiforov_beda.software <michail@beda.software>
2023-04-12 21:37:14 +0200

    Add port to app

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import asyncio
+import os
+
 from threading import Thread
 from flask import Flask
 
 from bots.big_values import get_binance_big_values
 
+
+PORT = os.environ.get("PORT", 8000)
 
 app = Flask(__name__)
 
@@ -22,7 +26,7 @@ if __name__ == "__main__":
 
     # Start the Flask app in a separate thread
     flask_thread = Thread(
-        target=app.run, kwargs={"debug": False, "host": "0.0.0.0", "port": "8000"}
+        target=app.run, kwargs={"debug": False, "host": "0.0.0.0", "port": PORT}
     )
     flask_thread.start()
 
